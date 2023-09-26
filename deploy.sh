7186435f7978aad7b56f8027229a0c48046f5585
Andreev Alexander <andreev.business.mail@gmail.com>
2023-09-26 17:34:23 +0300

    updated -o

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ chmod 600 ~/.ssh/id_rsa
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 ssh-keyscan -H $VM2_IP >> ~/.ssh/known_hosts
-sshpass -p $VM2_PASSWORD ssh -o StrictHostKeyChecking=no root@$VM2_IP "
+sshpass -p $VM2_PASSWORD ssh root@$VM2_IP "
 echo "$VM2_PASSWORD" | sudo -S ls
 cd rsoi-2023-lab1-ci-cd-AndreevAA
 sudo docker-compose up --build -d
