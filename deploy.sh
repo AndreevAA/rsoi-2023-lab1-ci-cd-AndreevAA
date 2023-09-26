0863e4aa7d7eb7f495aadfde77bdf0f1a3eba6bc
Andreev Alexander <andreev.business.mail@gmail.com>
2023-09-26 17:31:12 +0300

    updated doploy script

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ chmod 600 ~/.ssh/id_rsa
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 ssh-keyscan -H $VM2_IP >> ~/.ssh/known_hosts
-sshpass -p $VM2_PASSWORD ssh -o StrictHostKeyChecking=no user@$VM2_IP "
+sshpass -p $VM2_PASSWORD ssh -o StrictHostKeyChecking=no root@$VM2_IP "
 echo "$VM2_PASSWORD" | sudo -S ls
-cd BMSTU_reports_system
+cd rsoi-2023-lab1-ci-cd-AndreevAA
 sudo docker-compose up --build -d
 exit 0
 "  
